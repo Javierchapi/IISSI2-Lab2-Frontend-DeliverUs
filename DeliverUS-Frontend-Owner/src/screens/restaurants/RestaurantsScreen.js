82fbf6d3d963c3efdf6bb137e08498b6193a9bc7
Javier <javchagar@alum.us.es>
2026-03-18 17:54:53 +0100

    Initial commit

---
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantsScreen.js
@@ -0,0 +1,92 @@
+import { useContext, useEffect, useState } from 'react'
+import { StyleSheet, FlatList, Pressable, View } from 'react-native'
+
+import { getAll } from '../../api/RestaurantEndpoints'
+import ImageCard from '../../components/ImageCard'
+import TextSemiBold from '../../components/TextSemiBold'
+import TextRegular from '../../components/TextRegular'
+import * as GlobalStyles from '../../styles/GlobalStyles'
+import restaurantLogo from '../../../assets/restaurantLogo.jpeg'
+import { API_BASE_URL } from '@env'
+
+export default function RestaurantsScreen({ navigation, route }) {
+  const [restaurants, setRestaurants] = useState([])
+
+  useEffect(() => {
+    console.log('Loading restaurants, please wait 2 seconds')
+    setTimeout(() => {
+      setRestaurants(getAll) // getAll function has to be imported
+      console.log('Restaurants loaded')
+    }, 2000)
+  }, [])
+
+
+  const renderRestaurant = ({ item }) => {
+    return (
+      <ImageCard
+        imageUri={item.logo ? { uri: API_BASE_URL + '/' + item.logo } : restaurantLogo}
+        title={item.name}
+        onPress={() => {
+          navigation.navigate('RestaurantDetailScreen', { id: item.id })
+        }}
+      >
+        <TextRegular numberOfLines={2}>{item.description}</TextRegular>
+        {item.averageServiceMinutes !== null &&
+          <TextSemiBold>Avg. service time: <TextSemiBold textStyle={{ color: GlobalStyles.brandPrimary }}>{item.averageServiceMinutes} min.</TextSemiBold></TextSemiBold>
+        }
+        <TextSemiBold>Shipping: <TextSemiBold textStyle={{ color: GlobalStyles.brandPrimary }}>{item.shippingCosts.toFixed(2)}€</TextSemiBold></TextSemiBold>
+      </ImageCard>
+    )
+  }
+
+
+  return (
+    <FlatList
+      style={styles.container}
+      data={restaurants}
+      renderItem={renderRestaurant}
+      keyExtractor={item => item.id.toString()}
+    />
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  button: {
+    borderRadius: 8,
+    height: 40,
+    marginTop: 12,
+    padding: 10,
+    alignSelf: 'center',
+    flexDirection: 'row',
+    width: '80%'
+  },
+  actionButton: {
+    borderRadius: 8,
+    height: 40,
+    marginTop: 12,
+    margin: '1%',
+    padding: 10,
+    alignSelf: 'center',
+    flexDirection: 'column',
+    width: '50%'
+  },
+  actionButtonsContainer: {
+    flexDirection: 'row',
+    bottom: 5,
+    position: 'absolute',
+    width: '90%'
+  },
+  text: {
+    fontSize: 16,
+    color: 'white',
+    alignSelf: 'center',
+    marginLeft: 5
+  },
+  emptyList: {
+    textAlign: 'center',
+    padding: 50
+  }
+})
